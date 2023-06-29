5cea4534c52b395faf6dfedead220ac9f895da3f
jasperayawan <ejayawan22@gmail.com>
2023-06-29 20:38:11 +0800

    update search data

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const displayData = document.getElementById('display-data');
+const searchInput = document.getElementById('input_search');
 
 const fetchData = async () => {
   try {
@@ -6,19 +7,39 @@ const fetchData = async () => {
     const data = await response.json();
     const rates = data.rates;
 
-    Object.keys(rates).forEach((key) => {
-      const { name, unit, value, type } = rates[key];
+    const renderRates = (ratesData) => {
+      displayData.innerHTML = '';
 
-      displayData.innerHTML += `
-        <div>
-          <ul>
-            <li>Name: ${name}</li>
-            <li>Unit: ${unit}</li>
-            <li>Value: ${value}</li>
-            <li>Type: ${type}</li>
-          </ul>
-        </div>
-      `;
+      Object.keys(ratesData).forEach((key) => {
+        const { name, unit, value, type } = ratesData[key];
+
+        displayData.innerHTML += `
+          <div class="container-data">
+              <div class="row">
+                  <div></div>
+                  <div class="">
+                      <h1 class="rate">Rate: ${value}</h1>
+                      <p>Crypto name: ${name}</p>
+                      <p>Crypto unit: ${unit}</p>
+                  </div>
+              </div>
+          </div>
+        `;
+      });
+    };
+
+    renderRates(rates);
+
+    searchInput.addEventListener('input', () => {
+      const searchTerm = searchInput.value.toLowerCase();
+      const filteredRates = Object.keys(rates).reduce((result, key) => {
+        if (rates[key].name.toLowerCase().includes(searchTerm)) {
+          result[key] = rates[key];
+        }
+        return result;
+      }, {});
+
+      renderRates(filteredRates);
     });
   } catch (err) {
     console.log(err);
